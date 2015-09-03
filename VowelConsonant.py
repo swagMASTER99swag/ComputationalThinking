2ac9edb16a188e1f6172fe42de12233ac994f65d
Braxton <br56me43@isdedu.de>
2015-09-03 15:32:33 +0200

    Completed the Program \IBINswagMaster99swag

--- VowelConsonant.py
+++ VowelConsonant.py
@@ -1,1 +1,17 @@
-print ("hello world!")
+userInput = input("Enter your sentence: ")
+vowels="AEIOUaeiou"
+
+displayVowels=""
+displayConsonants=""
+
+for letter in userInput:
+    if letter in vowels:
+        displayVowels = displayVowels + letter
+    else:
+        displayConsonants = displayConsonants + letter
+
+print("Vowels: " + displayVowels)
+print("Consonants: " + displayConsonants)
+
+
+
